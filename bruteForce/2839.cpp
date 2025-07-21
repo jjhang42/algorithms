de2ec585de4e9f43e9d13424a22c8f6c19404448
JaeYeul Jhang <jaeyeuljhang@JaeYeuls-MacBook-Pro.local>
2025-07-22 00:18:25 +0900

    solved 2839

--- bruteForce/2839.cpp
+++ bruteForce/2839.cpp
@@ -3,10 +3,24 @@
 int	main(void)
 {
 	int	N;
-	int	x = 0;
-	int	y = 0;
-
+	int	x;
+	int	y;
 	std::cin >> N;
-	
+	int	minBags = N;
+
+	for (x = N / 5; 0 <= x; x--)
+	{
+		int	tem = N - x * 5;
+		if (tem % 3 == 0)
+		{
+			y = tem / 3;
+			break;
+		}
+	}
+	if ((x * 5 + y * 3) != N)
+		minBags = -1;
+	else
+		minBags = x + y;
+	std::cout << minBags;
 	return (0);
 }
